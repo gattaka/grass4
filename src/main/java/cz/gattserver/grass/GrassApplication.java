907f8b5d571b1fd59347730ea855bf2671b9375e
Hynek Uhlíř <hynek.uhlir@gmail.com>
2022-10-16 22:23:59 +0200

    SpringInject fixed

--- src/main/java/cz/gattserver/grass/GrassApplication.java
+++ src/main/java/cz/gattserver/grass/GrassApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"cz.gattserver.grass", "cz.gattserver.web"})
 @Import({ BaseConfig.class })
 public class GrassApplication {
 
