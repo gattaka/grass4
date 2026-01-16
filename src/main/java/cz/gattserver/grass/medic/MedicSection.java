126ba30db3a22948978b4af0fe778a4d7061d3ed
hynek <gattakahynca@seznam.cz>
2026-01-16 21:46:32 +0100

    Code format

--- src/main/java/cz/gattserver/grass/medic/MedicSection.java
+++ src/main/java/cz/gattserver/grass/medic/MedicSection.java
@@ -19,47 +19,45 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-
 @Component("medicSection")
 public class MedicSection implements SectionService {
 
-	private static Logger logger = LoggerFactory.getLogger(MedicSection.class);
+    private static Logger logger = LoggerFactory.getLogger(MedicSection.class);
 
-	private static final long ONCE_PER_DAY = 1L * 1000 * 60 * 60 * 24;
+    private static final long ONCE_PER_DAY = 1L * 1000 * 60 * 60 * 24;
 
-	@Resource(name = "medicPageFactory")
-	private PageFactory medicPageFactory;
+    @Resource(name = "medicPageFactory")
+    private PageFactory medicPageFactory;
 
-	@Autowired
-	private MedicEmailNotifier emailNotifier;
+    @Autowired
+    private MedicEmailNotifier emailNotifier;
 
-	@PostConstruct
-	private final void init() {
-		logger.info("MedicSection init");
-		TimerTask fetchMail = emailNotifier.getTimerTask();
-		Timer timer = new Timer();
-		LocalDateTime ldt = LocalDateTime.now().plusDays(1).withHour(4);
-		Date tomorrowMorning4am = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
-		timer.scheduleAtFixedRate(fetchMail, tomorrowMorning4am, ONCE_PER_DAY);
-	}
+    @PostConstruct
+    private void init() {
+        logger.info("MedicSection init");
+        TimerTask fetchMail = emailNotifier.getTimerTask();
+        Timer timer = new Timer();
+        LocalDateTime ldt = LocalDateTime.now().plusDays(1).withHour(4);
+        Date tomorrowMorning4am = Date.from(ldt.atZone(ZoneId.systemDefault()).toInstant());
+        timer.scheduleAtFixedRate(fetchMail, tomorrowMorning4am, ONCE_PER_DAY);
+    }
 
-	public boolean isVisibleForRoles(Set<Role> roles) {
-		if (roles == null)
-			return false;
-		return roles.contains(CoreRole.ADMIN);
-	}
+    public boolean isVisibleForRoles(Set<Role> roles) {
+        if (roles == null) return false;
+        return roles.contains(CoreRole.ADMIN);
+    }
 
-	public PageFactory getSectionPageFactory() {
-		return medicPageFactory;
-	}
+    public PageFactory getSectionPageFactory() {
+        return medicPageFactory;
+    }
 
-	public String getSectionCaption() {
-		return "Medic";
-	}
+    public String getSectionCaption() {
+        return "Medic";
+    }
 
-	@Override
-	public Role[] getSectionRoles() {
-		return new Role[0];
-	}
+    @Override
+    public Role[] getSectionRoles() {
+        return new Role[0];
+    }
 
 }
