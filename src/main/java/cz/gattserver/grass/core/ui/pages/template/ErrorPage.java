f482d61b61dc8417a8f282412a14f33266664907
hynek <gattakahynca@seznam.cz>
2026-01-30 14:02:55 +0100

    ErrorPage page routing

--- src/main/java/cz/gattserver/grass/core/ui/pages/template/ErrorPage.java
+++ src/main/java/cz/gattserver/grass/core/ui/pages/template/ErrorPage.java
@@ -1,7 +1,10 @@
 package cz.gattserver.grass.core.ui.pages.template;
 
+import com.vaadin.flow.router.*;
+import cz.gattserver.common.ui.ComponentFactory;
 import cz.gattserver.grass.core.exception.GrassPageException;
 import cz.gattserver.common.exception.SystemException;
+import cz.gattserver.grass.core.ui.pages.MainView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,92 +13,92 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.textfield.TextArea;
-import com.vaadin.flow.router.BeforeEnterEvent;
-import com.vaadin.flow.router.ErrorParameter;
-import com.vaadin.flow.router.HasErrorParameter;
 
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 
-@Tag(Tag.DIV)
-public class ErrorPage extends OneColumnPage implements HasErrorParameter<Exception> {
+@ParentLayout(MainView.class)
+public class ErrorPage extends Div implements HasErrorParameter<Exception> {
 
-	private static final long serialVersionUID = 4576353466500365046L;
+    private static final long serialVersionUID = 4576353466500365046L;
 
-	private static final Logger logger = LoggerFactory.getLogger(ErrorPage.class);
+    private static final Logger logger = LoggerFactory.getLogger(ErrorPage.class);
 
-	private GrassPageException exception;
+    private GrassPageException exception;
 
-	@Override
-	public int setErrorParameter(BeforeEnterEvent event, ErrorParameter<Exception> parameter) {
-		Throwable ex = parameter.getException();
-		while (true) {
-			if (ex.getCause() == null) break;
-			ex = ex.getCause();
-		}
-		if (ex instanceof GrassPageException) {
-			exception = (GrassPageException) ex;
-		} else {
-			exception = new GrassPageException(500, parameter.getException());
-		}
-		init();
-		return exception.getStatus();
-	}
+    @Override
+    public int setErrorParameter(BeforeEnterEvent event, ErrorParameter<Exception> parameter) {
+        Throwable ex = parameter.getException();
+        while (true) {
+            if (ex.getCause() == null) break;
+            ex = ex.getCause();
+        }
+        if (ex instanceof GrassPageException) {
+            exception = (GrassPageException) ex;
+        } else {
+            exception = new GrassPageException(500, parameter.getException());
+        }
 
-	@Override
-	protected void createColumnContent(Div layout) {
-		HorizontalLayout horizontalLayout = new HorizontalLayout();
-		horizontalLayout.setSpacing(true);
-		horizontalLayout.setPadding(true);
-		horizontalLayout.setWidthFull();
+        removeAll();
+        ComponentFactory componentFactory = new ComponentFactory();
 
-		Div div = new Div();
-		div.setText(getErrorText(exception.getStatus()));
-		div.addClassName("error-label");
-		Image img = new Image(getErrorImage(exception.getStatus()), "Chyba");
+        Div layout = componentFactory.createOneColumnLayout();
+        add(layout);
 
-		horizontalLayout.add(img);
-		horizontalLayout.add(div);
-		horizontalLayout.setFlexGrow(1, div);
-		horizontalLayout.setDefaultVerticalComponentAlignment(Alignment.CENTER);
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+        horizontalLayout.setSpacing(true);
+        horizontalLayout.setPadding(true);
+        horizontalLayout.setWidthFull();
 
-		layout.add(horizontalLayout);
+        Div div = new Div();
+        div.setText(getErrorText(exception.getStatus()));
+        div.addClassName("error-label");
+        Image img = new Image(getErrorImage(exception.getStatus()), "Chyba");
 
-		if (exception.getStatus() == 500) {
-			TextArea detailsArea = new TextArea();
-			String log = new SystemException("V aplikaci došlo k neočekávané chybě", exception).toString();
-			logger.error(log);
-			detailsArea.setValue(log);
-			detailsArea.setEnabled(true);
-			detailsArea.setReadOnly(true);
-			detailsArea.setWidthFull();
-			detailsArea.addClassName("error-text-field");
-			detailsArea.setHeight("500px");
-			detailsArea.getStyle().set("margin-top", "10px");
-			layout.add(detailsArea);
-		}
-	}
+        horizontalLayout.add(img);
+        horizontalLayout.add(div);
+        horizontalLayout.setFlexGrow(1, div);
+        horizontalLayout.setDefaultVerticalComponentAlignment(Alignment.CENTER);
 
-	protected String getErrorText(int status) {
-		switch (status) {
-			case 403:
-				return "403 - Nemáte oprávnění k provedení této operace";
-			case 404:
-				return "404 - Hledaný obsah neexistuje";
-			case 500:
-			default:
-				return "500 - Došlo k chybě na straně serveru";
-		}
-	}
+        layout.add(horizontalLayout);
 
-	protected String getErrorImage(int status) {
-		switch (status) {
-			case 403:
-				return "img/403.png";
-			case 404:
-				return "img/404.png";
-			case 500:
-			default:
-				return "img/500.png";
-		}
-	}
+        if (exception.getStatus() == 500) {
+            TextArea detailsArea = new TextArea();
+            String log = new SystemException("V aplikaci došlo k neočekávané chybě", exception).toString();
+            logger.error(log);
+            detailsArea.setValue(log);
+            detailsArea.setEnabled(true);
+            detailsArea.setReadOnly(true);
+            detailsArea.setWidthFull();
+            detailsArea.addClassName("error-text-field");
+            detailsArea.setHeight("500px");
+            detailsArea.getStyle().set("margin-top", "10px");
+            layout.add(detailsArea);
+        }
+
+        return exception.getStatus();
+    }
+
+    protected String getErrorText(int status) {
+        switch (status) {
+            case 403:
+                return "403 - Nemáte oprávnění k provedení této operace";
+            case 404:
+                return "404 - Hledaný obsah neexistuje";
+            case 500:
+            default:
+                return "500 - Došlo k chybě na straně serveru";
+        }
+    }
+
+    protected String getErrorImage(int status) {
+        switch (status) {
+            case 403:
+                return "img/403.png";
+            case 404:
+                return "img/404.png";
+            case 500:
+            default:
+                return "img/500.png";
+        }
+    }
 }
