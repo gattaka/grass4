9563dd03e6dbd49340a186df87a25a775faf4b0a
hynek <gattakahynca@seznam.cz>
2026-02-21 00:10:05 +0100

    Songs print two-column variant

--- src/main/java/cz/gattserver/grass/songs/facades/impl/SongsFacadeImpl.java
+++ src/main/java/cz/gattserver/grass/songs/facades/impl/SongsFacadeImpl.java
@@ -16,6 +16,7 @@ import cz.gattserver.grass.songs.model.interfaces.SongOverviewTO;
 import cz.gattserver.grass.songs.model.interfaces.SongTO;
 import cz.gattserver.grass.songs.util.Mapper;
 import org.apache.commons.io.IOUtils;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,133 +27,139 @@ import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Transactional
 @Component
 public class SongsFacadeImpl implements SongsService {
 
-	@Autowired
-	private Mapper mapper;
+    @Autowired
+    private Mapper mapper;
 
-	@Autowired
-	private SongsRepository songsRepository;
+    @Autowired
+    private SongsRepository songsRepository;
 
-	@Autowired
-	private ChordsRepository chordsRepository;
+    @Autowired
+    private ChordsRepository chordsRepository;
 
     @Autowired
     private ExportsService exportsService;
 
-	@Override
-	public SongTO getSongById(Long id) {
-		Song song = songsRepository.findById(id).orElse(null);
-		if (song == null)
-			return null;
-		return mapper.mapSong(song);
-	}
+    @Override
+    public SongTO getSongById(Long id) {
+        Song song = songsRepository.findById(id).orElse(null);
+        if (song == null) return null;
+        return mapper.mapSong(song);
+    }
 
-	@Override
-	public SongTO saveSong(SongTO to) {
-		Song song = mapper.mapSong(to);
-		song.setText(eolToBreakline(to.getText()));
-		song = songsRepository.save(song);
-		return mapper.mapSong(song);
-	}
+    @Override
+    public SongTO saveSong(SongTO to) {
+        Song song = mapper.mapSong(to);
+        song.setText(eolToBreakline(to.getText()));
+        song = songsRepository.save(song);
+        return mapper.mapSong(song);
+    }
 
-	@Override
-	public String breaklineToEol(String text) {
-		return text.replace("<br/>", "" + '\n').replace("<br>", "" + '\n');
-	}
+    @Override
+    public String breaklineToEol(String text) {
+        return text.replace("<br/>", "" + '\n').replace("<br>", "" + '\n');
+    }
 
-	@Override
-	public String eolToBreakline(String text) {
-		text = text.replace("\r\n", "<br/>");
-		return text.replace("\n", "<br/>");
-	}
+    @Override
+    public String eolToBreakline(String text) {
+        text = text.replace("\r\n", "<br/>");
+        return text.replace("\n", "<br/>");
+    }
 
-	@Override
-	public int getSongsCount(SongOverviewTO filterTO) {
-		return (int) songsRepository.count(filterTO);
-	}
+    @Override
+    public int getSongsCount(SongOverviewTO filterTO) {
+        return (int) songsRepository.count(filterTO);
+    }
 
-	private List<QuerySortOrder> ensureSort(List<QuerySortOrder> sortOrders) {
-		// Řeší problém s nekonzistencí pořadí záznamů v celém selektu a v selektu id -- problém se projevuje, když se
-		// provede sort dle sloupce, který má více shodných hodnot -- tyto "duplicity" pak nemají z pohledu DB zřejmě
-		// dané pořadí a tak mohou být jiné mezi jednotlivými selekty
-		for (QuerySortOrder q : sortOrders)
-			if (q.getSorted().equals("id"))
-				return sortOrders;
+    private List<QuerySortOrder> ensureSort(List<QuerySortOrder> sortOrders) {
+        // Řeší problém s nekonzistencí pořadí záznamů v celém selektu a v selektu id -- problém se projevuje, když se
+        // provede sort dle sloupce, který má více shodných hodnot -- tyto "duplicity" pak nemají z pohledu DB zřejmě
+        // dané pořadí a tak mohou být jiné mezi jednotlivými selekty
+        for (QuerySortOrder q : sortOrders)
+            if (q.getSorted().equals("id")) return sortOrders;
 
-		sortOrders.add(new QuerySortOrder("id", SortDirection.DESCENDING));
-		return sortOrders;
-	}
+        sortOrders.add(new QuerySortOrder("id", SortDirection.DESCENDING));
+        return sortOrders;
+    }
 
-	@Override
-	public List<SongOverviewTO> getSongs(
-			SongOverviewTO filterTO, int offset, int limit,
-			List<QuerySortOrder> sortOrders) {
-		return songsRepository.findSongs(filterTO, offset, limit,
-				QuerydslUtil.transformOrdering(ensureSort(sortOrders),
-						s -> s));
-	}
+    @Override
+    public List<SongOverviewTO> getSongs(SongOverviewTO filterTO, int offset, int limit,
+                                         List<QuerySortOrder> sortOrders) {
+        return songsRepository.findSongs(filterTO, offset, limit,
+                QuerydslUtil.transformOrdering(ensureSort(sortOrders), s -> s));
+    }
 
-	@Override
-	public List<Long> getSongsIds(SongOverviewTO filterTO, List<QuerySortOrder> sortOrders) {
-		return songsRepository.findSongsIds(filterTO, QuerydslUtil.transformOrdering(ensureSort(sortOrders),
-				s -> s));
-	}
+    @Override
+    public List<Long> getSongsIds(SongOverviewTO filterTO, List<QuerySortOrder> sortOrders) {
+        return songsRepository.findSongsIds(filterTO, QuerydslUtil.transformOrdering(ensureSort(sortOrders), s -> s));
+    }
 
-	@Override
-	public void deleteSong(Long id) {
-		songsRepository.deleteById(id);
-	}
+    @Override
+    public void deleteSong(Long id) {
+        songsRepository.deleteById(id);
+    }
 
-	@Override
-	public SongTO importSong(InputStream in, String fileName) {
-		SongTO to = SongFileParser.parseSongInfo(fileName);
-		try {
-			to.setText(IOUtils.toString(in, "cp1250"));
-		} catch (IOException e) {
-			to.setText("Nezdařilo se zpracovat obsah souboru");
-		}
-		return saveSong(to);
-	}
+    @Override
+    public SongTO importSong(InputStream in, String fileName) {
+        SongTO to = SongFileParser.parseSongInfo(fileName);
+        try {
+            to.setText(IOUtils.toString(in, "cp1250"));
+        } catch (IOException e) {
+            to.setText("Nezdařilo se zpracovat obsah souboru");
+        }
+        return saveSong(to);
+    }
 
-	@Override
-	public ChordTO saveChord(ChordTO to) {
-		Chord chord = mapper.mapChord(to);
-		chord = chordsRepository.save(chord);
-		to.setId(chord.getId());
-		return to;
-	}
+    @Override
+    public ChordTO saveChord(ChordTO to) {
+        Chord chord = mapper.mapChord(to);
+        chord = chordsRepository.save(chord);
+        to.setId(chord.getId());
+        return to;
+    }
 
-	@Override
-	public void deleteChord(Long id) {
-		chordsRepository.deleteById(id);
-	}
+    @Override
+    public void deleteChord(Long id) {
+        chordsRepository.deleteById(id);
+    }
 
-	@Override
-	public List<ChordTO> getChords(ChordTO filterTO) {
-		return mapper.mapChords(chordsRepository.findAllOrderByName(filterTO));
-	}
+    @Override
+    public List<ChordTO> getChords(ChordTO filterTO) {
+        return mapper.mapChords(chordsRepository.findAllOrderByName(filterTO));
+    }
 
-	@Override
-	public ChordTO getChordById(Long id) {
-		return mapper.mapChord(chordsRepository.findById(id).orElse(null));
-	}
+    @Override
+    public ChordTO getChordById(Long id) {
+        return mapper.mapChord(chordsRepository.findById(id).orElse(null));
+    }
 
-	@Override
-	public ChordTO getChordByName(String name) {
-		return mapper.mapChord(chordsRepository.findByName(name));
-	}
+    @Override
+    public ChordTO getChordByName(String name) {
+        return mapper.mapChord(chordsRepository.findByName(name));
+    }
 
     @Override
     public Path print(SongTO s, boolean twoColumn) {
         var ctx = new Context();
         ctx.setVariable("name", s.getName());
-        ctx.setVariable("text", s.getText());
         ctx.setVariable("author", s.getAuthor());
+        ctx.setVariable("columnsCount", twoColumn ? 2 : 1);
+        if (twoColumn) {
+            List<String> lines = List.of(s.getText().split("\n"));
+            int half = lines.size() / 2;
+            String textCol1 = Strings.join(lines.subList(0, half), '\n');
+            String textCol2 = Strings.join(lines.subList(half, lines.size()), '\n');
+            ctx.setVariable("textCol1", textCol1);
+            ctx.setVariable("textCol2", textCol2);
+        } else {
+            ctx.setVariable("text", s.getText());
+        }
 
-        return exportsService.createPDFReport(ctx, "song-one-col");
+        return exportsService.createPDFReport(ctx, "song");
     }
 }
